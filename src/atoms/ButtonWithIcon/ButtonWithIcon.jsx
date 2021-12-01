c908ee4373acd78d7b594cb560c9f5e22aa83d25
nikita <nikita.grishin@corp.codeinside.ru>
2021-12-01 12:01:12 +0300

    added buttom with icon atom

---
+++ src/atoms/ButtonWithIcon/ButtonWithIcon.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import style from "./ButtonWithIcon.module.css";
+
+export default class ButtonWithIcon extends React.Component {
+    render() {
+        this.onCLickHandler = this.props.onClick;
+        this.icon = this.props.icon;
+
+        return( 
+            <div className={style.button} onClick={this.onClick}> 
+                <img className={style.icon} src={this.icon} />
+            </div>
+        )
+    }
+}
