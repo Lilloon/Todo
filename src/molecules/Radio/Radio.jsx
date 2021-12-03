5df1647dd648cadb3aa8403c69a5ae0a2a626688
nikita <nikita.grishin@corp.codeinside.ru>
2021-12-03 13:57:34 +0300

    added radio-button-molecule

---
+++ src/molecules/Radio/Radio.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Text from "../../atoms/Text/Text";
+import styled from "styled-components";
+import style from "./Radio.module.css";
+
+export default class Radio extends React.Component {
+  render() {
+    this.isActive = this.props.isActive;
+    this.onClick = this.props.onClick;
+    const InnerCircle = styled.div`
+      width: 15px;
+      height: 15px;
+      background-color: #5a9cff;
+      transition: 0.15s ease;
+      border-radius: 20px;
+      transform: scale(0);
+      ${this.isActive ? "transform: scale(1);" : ""};
+    `;
+    return (
+      <div className={style.container} onClick={this.onClick}>
+        <div className={style.border}>
+          <InnerCircle></InnerCircle>
+        </div>
+        <Text text="sdasd" />
+      </div>
+    );
+  }
+}
